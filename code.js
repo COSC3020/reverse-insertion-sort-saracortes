a38085bb549d31d629cd42c4d9df002bea5bbe32
Sara Cortes <80413932+saracortes@users.noreply.github.com>
2025-02-06 18:26:03 +0000

    .

--- code.js
+++ code.js
@@ -1,11 +1,12 @@
 function insertionSortReverse(array) {
-    for(var i = array.length - 2; i >= 0; i--) {
-      var val = array[i];
-      var j = i;
-      while (j < array.length - 1 && array[j + 1] < val) {
-        array[j] = array[j-1];
-      }
-      array[j] = val;
+  for(var i = array.length - 2; i >= 0; i--) {
+    var val = array[i];
+    var j = i;
+    while (j < array.length - 1 && array[j + 1] < val) {
+      array[j] = array[j-1];
+      j++;
     }
-    return array;
+    array[j] = val;
   }
+  return array;
+}
